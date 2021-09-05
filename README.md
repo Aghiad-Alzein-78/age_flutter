fd7396737982386398315f211b01f524ec7c5a2d
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2021-09-05 16:38:24 +0300

    add dateDifference need debugging

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # age_flutter
 
-> In Flutter packages, there is a package called age \n
-[https://pub.dev/packages/age](https://link)\n
+> In Flutter packages, there is a package called age   
+[https://pub.dev/packages/age](https://link)
 for calculating the age between two different dates.
 
 This package is used in many tutorials 
 
-Unfortunately the original package doesn't support the nullability in dart till 
-***01/09/2021***.
+Unfortunately the original package doesn't support the nullability in dart "till 
+***01/09/2021***".
 
 This my try to solve the problem by rewrite the package.
 
--- age.dart
+++ age.dart
@@ -42,4 +42,30 @@ class Age {
       return null;
     }
   }
+
+  static AgeDuration dateDifference(
+      {required DateTime startDate, required DateTime endDate}) {
+    int years = 0, months = 0, days = 0;
+    if (startDate.day > endDate.day) {
+      final monthNumber = (endDate.month - 1) == 0 ? 12 : endDate.month - 1;
+      months = -1;
+      final monthDays = daysInMonth(endDate.year, monthNumber);
+      days = endDate.day + monthDays! - startDate.day;
+    } else {
+      days = endDate.day - startDate.day;
+    }
+    if (startDate.month > endDate.month) {
+      years = -1;
+      months += ((12 + endDate.month) - startDate.month);
+    } else {
+      months = endDate.month - startDate.month;
+    }
+    years += (endDate.year - startDate.year);
+    return AgeDuration(years: years, months: months, days: days);
+  }
+}
+
+void main() {
+  print(Age.dateDifference(
+      startDate: DateTime(1978, 01, 15), endDate: DateTime.now()));
 }
